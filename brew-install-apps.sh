24caa9c6ff18a9890acdb307b75217215b84712e
WouterDS <wouter.de.schuyter@gmail.com>
2016-12-19 10:35:40 +0100

    feat(brew): add script installing GUI apps via Homebrew Cask
    brew-install-apps.sh taps caskroom/versions and installs the desktop applications with
    --appdir=/Applications so they land in the system Applications folder rather than ~/Applications.
    
    Covers editors and IDEs (Atom, Sublime Text, PHPStorm), virtualisation (VirtualBox, Vagrant),
    and utilities such as Dropbox, Spotify, Skype, Spectacle, Alfred, Sketch and Sequel Pro.

---
+++ brew-install-apps.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Caskroom versions
+brew tap caskroom/versions
+
+# Dropbox
+brew cask install dropbox --appdir=/Applications
+
+# Keeping You Awake
+brew cask install keepingyouawake --appdir=/Applications
+
+# Virtual box & Vagrant Manager
+brew cask install virtualbox --appdir=/Applications
+brew cask install vagrant
+brew cask install vagrant-manager --appdir=/Applications
+
+# Atom
+brew cask install atom --appdir=/Applications
+
+# Sublime text
+brew cask install sublime-text --appdir=/Applications
+
+# PHPStorm
+brew cask install phpstorm --appdir=/Applications
+
+# Spotify
+brew cask install spotify --appdir=/Applications
+
+# Spotify notifications
+brew cask install spotify-notifications --appdir=/Applications
+
+# Skype
+brew cask install skype --appdir=/Applications
+
+# Transmission
+brew cask install transmission --appdir=/Applications
+
+# Spectacle
+brew cask install spectacle --appdir=/Applications
+
+# Viscosity
+brew cask install viscosity --appdir=/Applications
+
+# Sequel Pro
+brew cask install sequel-pro-nightly --appdir=/Applications
+
+# SmcFanControl
+brew cask install smcfancontrol --appdir=/Applications
+
+# Sketch
+brew cask install sketch --appdir=/Applications
+
+# Kibako
+brew cask install kibako --appdir=/Applications
+
+# Alfred
+brew cask install alfred --appdir=/Applications
+
+# Steelseries
+brew cask install steelseries-engine --appdir=/Applications
+
+# Knock to unlock
+brew cask install knock --appdir=/Applications
+
+# Application loader
+brew cask install application-loader --appdir=/Applications
+
+# Rescue Time
+brew cask install rescuetime --appdir=/Applications
+
+# VideoLanPlayer (aka VLC)
+brew cask install vlc --appdir=/Applications
+
+# Microsoft Silverlight
+brew cask install silverlight --appdir=/Applications
+
+# Bartender
+brew cask install bartender --appdir=/Applications
+
+# Charles
+brew cask install charles --appdir=/Applications
