e745812d1d24c83491c9399b5e172e8bed760594
WouterDS <wouter.de.schuyter@gmail.com>
2017-02-12 14:39:02 +0100

    Install brew

--- brew.sh
+++ brew.sh
@@ -6,6 +6,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Make sure we’re using the latest Homebrew
 brew update
 
