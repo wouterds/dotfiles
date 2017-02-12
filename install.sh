949fc949f3c9b2da9d4e49e05bd2cde0b822c991
wouterds <wouter.de.schuyter@gmail.com>
2017-02-12 18:13:42 +0100

    refactor(install): copy dotfiles after the installs instead of first
    Moves the rsync of the repository into the home directory from the top of the script to just
    before the preferences section, and moves the sudo prompt and keep-alive to sit before the
    Pygments and thefuck steps.
    
    The Pygments and thefuck steps append to ~/.aliases, so running them before the rsync meant
    their lines were overwritten by the copied .aliases. Syncing afterwards keeps them.

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Copy dotfiles
-cd "$(dirname "${BASH_SOURCE}")";
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
-	--exclude "init/" --exclude "README.md" -avh --no-perms . ~;
-source ~/.bash_profile;
-
 # Ask for the administrator password upfront.
 sudo -v
 
@@ -112,6 +106,12 @@ mas install 409203825 # Numbers
 # Cleanup
 brew cleanup
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Cat with syntax highlighting
 easy_install Pygments
 echo '' >> ~/.aliases
@@ -127,12 +127,11 @@ echo 'eval "$(thefuck --alias)"' >> ~/.aliases
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Copy dotfiles
+cd "$(dirname "${BASH_SOURCE}")";
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+	--exclude "init/" --exclude "README.md" -avh --no-perms . ~;
+source ~/.bash_profile;
 
 ###############################################################################
 # macOS preferences                                                           #
