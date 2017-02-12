5c0cf645205af884dd87ebb900b9a1e45963bddc
WouterDS <wouter.de.schuyter@gmail.com>
2017-02-12 14:55:55 +0100

    Added small tweaks to install.sh

--- install.sh
+++ install.sh
@@ -78,3 +78,15 @@ brew cask install bartender --appdir=/Applications
 
 # Cleanup
 brew cleanup
+
+# Cat with syntax highlighting
+easy_install Pygments
+echo '' >> ~/.aliases
+echo '# Cat with syntax highlighting' >> ~/.aliases
+echo 'alias cat="pygmentize -g"' >> ~/.aliases
+
+# The fuck
+brew install thefuck
+echo '' >> ~/.aliases
+echo '# Fuck alias' >> ~/.aliases
+echo 'eval "$(thefuck --alias)"' >> ~/.aliases
