14f8937fce4b2ca9ac30797e1e58619dc11d466a
wouterds <wouter.de.schuyter@gmail.com>
2017-02-22 11:00:21 +0100

    feat(atom): add script installing Atom packages via apm
    Adds install-apm-packages.sh installing the Atom editor packages with apm, each pinned to an
    exact version: atom-autocomplete-php, blame, language-blade, language-groovy, linter, linter-php,
    minimap, pigments and wakatime.

---
+++ install-apm-packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Atom packages
+apm install atom-autocomplete-php@0.22.0
+apm install blame@0.10.2
+apm install language-blade@0.28.0
+apm install language-groovy@0.6.0
+apm install linter@1.11.23
+apm install linter-php@1.3.1
+apm install minimap@4.26.8
+apm install pigments@0.39.0
+apm install wakatime@6.0.13
