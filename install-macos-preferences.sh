727be01109e475ddfb68b528ecf069943fa2eeed
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-07-06 18:26:38 +0200

    feat(macos): quit the printer app when its jobs finish
    Sets "Quit When Finished" so the print queue window closes on its own instead of staying in the
    Dock after every print job.

--- install-macos-preferences.sh
+++ install-macos-preferences.sh
@@ -39,6 +39,9 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
