167d0bbabdbd95f672f3a0043d1f5e6e339c154e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-07-06 23:00:14 +0200

    Install gifcapture

--- install-brew-apps.sh
+++ install-brew-apps.sh
@@ -42,6 +42,7 @@ brew cask install etcher
 brew cask install macdown
 brew cask install kitematic
 brew cask install media-converter
+brew cask install gifcapture
 
 # Extensions & others
 brew cask install padbury-clock
