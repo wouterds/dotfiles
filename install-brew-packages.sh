07ab3c40c0ed54c82bfcd6d8d10be0cbd2db8889
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-02-11 13:14:33 +0100

    :bug: Removed some more deprecatred taps

--- install-brew-packages.sh
+++ install-brew-packages.sh
@@ -6,8 +6,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Brew versions
-brew tap homebrew/versions
+# We need this later on for php & composer
 brew tap homebrew/homebrew-php
 
 # Basics
