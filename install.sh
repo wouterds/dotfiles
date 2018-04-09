cb386fff0a305bed4d4806bd1e9cb4e79c531521
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-04-09 16:09:35 +0200

    :zap: brew upgrade --all was deprecated

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew upgrade --all
+brew upgrade
 
 # Install homebrew packages
 ./install-brew-packages.sh
