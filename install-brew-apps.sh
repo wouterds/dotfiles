54dc6846c2cd996f6da282940f7e39e73c1e2ecc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-06-19 08:51:24 +0200

    :bug: Don't install sequel pro nightly

--- install-brew-apps.sh
+++ install-brew-apps.sh
@@ -26,7 +26,7 @@ brew cask install whatsapp
 brew cask install transmission
 brew cask install spectacle
 brew cask install viscosity
-brew cask install sequel-pro-nightly
+brew cask install sequel-pro
 brew cask install sketch
 brew cask install zeplin
 brew cask install alfred2
