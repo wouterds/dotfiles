4ccf783e0c1238b35c5f00b7776b60767b8c725d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-07-25 18:07:34 +0200

    feat(vscode): add script installing VS Code extensions
    Adds install-code-packages.sh installing extensions through the `code` CLI: eslint, the Dracula
    theme, GitLens, EditorConfig, Flow and colour highlighting.
    
    Wires it into install.sh next to the Atom package step and adds it to the rsync exclude list.

---
+++ install-code-packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Code packages
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
+code --install-extension flowtype.flow-for-vscode
+code --install-extension naumovs.color-highlight
