190dcb043ca6864eede7afb53d5f3cd5810fcb21
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-08-10 18:31:03 +0200

    style(vscode): lowercase the extension identifiers
    Rewrites EditorConfig.EditorConfig and PeterJausovec.vscode-docker in lowercase. Marketplace
    identifiers are case-insensitive, so this only makes the list consistent with the other entries.

--- install-code-packages.sh
+++ install-code-packages.sh
@@ -4,7 +4,7 @@
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
-code --install-extension EditorConfig.EditorConfig
+code --install-extension editorconfig.editorconfig
 code --install-extension flowtype.flow-for-vscode
 code --install-extension naumovs.color-highlight
-code --install-extension PeterJausovec.vscode-docker
+code --install-extension peterjausovec.vscode-docker
