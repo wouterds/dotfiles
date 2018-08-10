12410878f2d2cb9d2c3c58ffb746afa3ddb4e1db
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-08-10 18:31:40 +0200

    feat(vscode): install prettier, php-intellisense, npm and CSS class extensions
    Adds four extensions: prettier for formatting, php-intellisense for PHP completion and navigation,
    vscode-npm-script for running scripts from package.json, and html-css-class-completion for
    completing class names from the project's stylesheets.

--- install-code-packages.sh
+++ install-code-packages.sh
@@ -8,3 +8,7 @@ code --install-extension editorconfig.editorconfig
 code --install-extension flowtype.flow-for-vscode
 code --install-extension naumovs.color-highlight
 code --install-extension peterjausovec.vscode-docker
+code --install-extension esbenp.prettier-vscode
+code --install-extension felixfbecker.php-intellisense
+code --install-extension eg2.vscode-npm-script
+code --install-extension zignd.html-css-class-completion
