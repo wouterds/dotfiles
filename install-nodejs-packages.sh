66c36cf7d85badbb2c48e6e510fe9bfcf94d61dc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-06-11 17:33:20 +0200

    :fire: Unused node packages

--- install-nodejs-packages.sh
+++ install-nodejs-packages.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 # Node packages
-npm install -g grunt-cli
-npm install -g gulp
-npm install -g bower
-npm install -g webpack
 npm install -g yarn
-npm install -g gtop
 npm install -g react-native-cli
-npm install -g whackage
 npm install -g ios-deploy
 npm install -g react-devtools
 npm install -g wml
-npm install -g serialport
