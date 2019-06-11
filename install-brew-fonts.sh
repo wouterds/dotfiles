bc27c693931d304cf9d6c67cdb973fb192ffce0e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-06-11 17:39:00 +0200

    feat(terminal): install the Impact, Public Sans, Raleway and Oswald fonts
    Adds four more font casks for design and document work, alongside the existing Open Sans, Roboto and
    Hack.

--- install-brew-fonts.sh
+++ install-brew-fonts.sh
@@ -10,6 +10,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew cask install caskroom/fonts/font-open-sans
 brew cask install caskroom/fonts/font-roboto
 brew cask install caskroom/fonts/font-hack
+brew cask install caskroom/fonts/font-impact
+brew cask install caskroom/fonts/font-public-sans
+brew cask install caskroom/fonts/font-raleway
+brew cask install caskroom/fonts/font-oswald
 
 # Cleanup
 brew cleanup
