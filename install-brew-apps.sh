3548415a2bcca49644f53c23aefe6df726288bb0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-06-30 21:19:16 +0200

    feat(apps): install OBS
    Adds the obs cask, Open Broadcaster Software, for screen recording and streaming.

--- install-brew-apps.sh
+++ install-brew-apps.sh
@@ -57,6 +57,7 @@ brew cask install fritzing
 brew cask install charles
 brew cask install diskmaker-x
 brew cask install twitch
+brew cask install obs
 brew cask install handbrake
 brew cask install ipvanish-vpn
 brew cask install cityofzion-neon
