ec1edb65f9505482e3955bc45631c8690502edf4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-17 18:58:10 +0200

    refactor(node): replace react-native-cli with the community CLI
    Switches the global react-native-cli package for @react-native-community/cli. React Native moved
    its CLI out of core into the community package, and the old global shim shadowed the per-project
    version.

--- install-nodejs-packages.sh
+++ install-nodejs-packages.sh
@@ -2,7 +2,7 @@
 
 # Node packages
 npm install -g yarn
-npm install -g react-native-cli
+npm install -g @react-native-community/cli
 npm install -g ios-deploy
 npm install -g wml
 npm install -g vtop
