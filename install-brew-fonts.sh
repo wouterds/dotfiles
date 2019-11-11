eb0202975e44bd4f729ebdf6c4585d0e983593f1
wouterds <wouter.de.schuyter@gmail.com>
2019-11-11 01:37:58 +0100

    :arrow_up: Fonts have been moved

--- install-brew-fonts.sh
+++ install-brew-fonts.sh
@@ -7,13 +7,13 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Fonts
-brew cask install caskroom/fonts/font-open-sans
-brew cask install caskroom/fonts/font-roboto
-brew cask install caskroom/fonts/font-hack
-brew cask install caskroom/fonts/font-impact
-brew cask install caskroom/fonts/font-public-sans
-brew cask install caskroom/fonts/font-raleway
-brew cask install caskroom/fonts/font-oswald
+brew cask install homebrew/cask-fonts/font-open-sans
+brew cask install homebrew/cask-fonts/font-roboto
+brew cask install homebrew/cask-fonts/font-hack
+brew cask install homebrew/cask-fonts/font-impact
+brew cask install homebrew/cask-fonts/font-public-sans
+brew cask install homebrew/cask-fonts/font-raleway
+brew cask install homebrew/cask-fonts/font-oswald
 
 # Cleanup
 brew cleanup
