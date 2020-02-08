c1df4c4b6b25c1b19bbc795b1b8de645d4dcc3f4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-02-08 18:22:19 +0100

    refactor(install): condense install.sh and drop the app restart loop
    Tightens the script by removing the per-step comments in favour of a flat list of the install-*.sh
    calls, and reorders it so the fonts run with the other Homebrew steps and the VS Code extensions run
    last, after the editor itself has been installed.
    
    Also drops `brew update` and `brew upgrade`, which the Homebrew install already performs, and the
    loop that killed Finder, the Dock and the rest. Restarting those every run was disruptive, and a
    logout achieves the same thing once.

--- install.sh
+++ install.sh
@@ -1,42 +1,23 @@
 #!/usr/bin/env bash
 
-# Ask for the administrator password upfront.
+# Privileges
 sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install macos preferences
+# macOS preferences
 ./install-macos-preferences.sh
 
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-brew upgrade
 
-# Install homebrew packages
 ./install-brew-packages.sh
-
-# Install homebrew apps
 ./install-brew-apps.sh
-
-# Install homebrew mac apps
 ./install-brew-store-apps.sh
-
-# Install nodejs packages
-./install-nodejs-packages.sh
-
-# Install Python packages
-./install-python-packages.sh
-
-# Install Code packages
-./install-code-packages.sh
-
-# Install ruby gems
-./install-ruby-gems.sh
-
-# Install homebrew fonts
 ./install-brew-fonts.sh
+./install-nodejs-packages.sh
+./install-python-packages.sh
+./install-ruby-gems.sh
+./install-code-packages.sh
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -65,10 +46,4 @@ rsync \
 
 rsync ./.vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-# Postinstall
 ./postinstall.sh
-
-# Restart apps
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" "iCal"; do
-  killall "${app}" > /dev/null 2>&1
-done
