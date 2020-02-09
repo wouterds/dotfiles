ab91d3a11852215a0a2c46bab94fb6d5882df4d8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-02-09 23:38:29 +0100

    refactor(vscode): drop the npm script extension
    Removes eg2.vscode-npm-script. Scripts are run from the terminal, so the sidebar view it added went
    unused.

--- postinstall-vscode.sh
+++ postinstall-vscode.sh
@@ -7,7 +7,6 @@ code --install-extension editorconfig.editorconfig
 code --install-extension flowtype.flow-for-vscode
 code --install-extension naumovs.color-highlight
 code --install-extension peterjausovec.vscode-docker
-code --install-extension eg2.vscode-npm-script
 code --install-extension zignd.html-css-class-completion
 code --install-extension ms-python.python
 code --install-extension mikestead.dotenv
