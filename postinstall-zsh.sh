294489a3dfc4647a195b4af0f9835df5cb4e25ef
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-02-14 00:03:35 +0100

    :wrench: Oh my ZSH!

--- postinstall-zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
