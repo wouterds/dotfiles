49976ab068839d60922e2ddc3d5415a50f9e1c38
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-07-14 14:01:40 +0200

    feat(apps): install Postman
    Adds the postman cask, alongside Paw, for testing APIs.

--- install-apps.sh
+++ install-apps.sh
@@ -47,6 +47,7 @@ brew cask install sonos
 brew cask install notion
 brew cask install hwsensors
 brew cask install paw
+brew cask install postman
 brew cask install betaflight-configurator
 brew cask install cityofzion-neon
 
