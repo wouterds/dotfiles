10d101392bcf774b495ca7452308bd35c7682067
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-05-07 10:16:39 +0200

    refactor(fonts): use brew install --cask for the font casks
    The font script still used the deprecated `brew cask install` form, missed when the other scripts
    were converted. Updates all eight font casks.

--- install-fonts.sh
+++ install-fonts.sh
@@ -5,14 +5,14 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Fonts
-brew cask install homebrew/cask-fonts/font-open-sans
-brew cask install homebrew/cask-fonts/font-roboto
-brew cask install homebrew/cask-fonts/font-hack
-brew cask install homebrew/cask-fonts/font-impact
-brew cask install homebrew/cask-fonts/font-public-sans
-brew cask install homebrew/cask-fonts/font-raleway
-brew cask install homebrew/cask-fonts/font-oswald
-brew cask install homebrew/cask-fonts/font-inter
+brew install --cask homebrew/cask-fonts/font-open-sans
+brew install --cask homebrew/cask-fonts/font-roboto
+brew install --cask homebrew/cask-fonts/font-hack
+brew install --cask homebrew/cask-fonts/font-impact
+brew install --cask homebrew/cask-fonts/font-public-sans
+brew install --cask homebrew/cask-fonts/font-raleway
+brew install --cask homebrew/cask-fonts/font-oswald
+brew install --cask homebrew/cask-fonts/font-inter
 
 # Cleanup
 brew cleanup
