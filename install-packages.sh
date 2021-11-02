68815fbb5e49fa0dbe5a306960b3b080580bc318
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-11-02 15:29:53 +0100

    :fire: Cleaned up some unused packages

--- install-packages.sh
+++ install-packages.sh
@@ -28,42 +28,25 @@ brew install screen
 brew install watchman
 brew install ssh-copy-id
 brew install diff-so-fancy
-brew install php72
-brew install composer
 brew install thefuck
-brew install certbot
 brew install docker-compose
 brew install watch
 brew install speedtest-cli
-brew install wakeonlan
 brew install mas
 brew install cask
 brew install tor
-brew install graphicsmagick
-brew install imagemagick
-brew install wpscan
-brew install glances
-brew install translate-shell
 brew install awscli
 brew install tig
-brew install hub
 brew install ffmpeg
 brew install gifski
 brew install ios-deploy
 brew install neofetch
-brew install arp-scan
-brew install googler
-brew install clementtsang/bottom/bottom
-brew install --cask ngrok
+brew install bottom
+brew install ngrok
 brew install adoptopenjdk8
 brew install gradle
 brew install android-sdk
-brew install --cask gdisk
-brew install qrencode
-brew install mkcert
-brew install p7zip
 brew install git-remote-codecommit
-brew install --cask robo-3t
 brew install tree
 brew install act
 
