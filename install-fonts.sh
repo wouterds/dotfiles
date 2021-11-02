51df3a7118c40996b86243ca950c2db84911fa85
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-11-02 15:44:37 +0100

    :fire: Dead fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -5,8 +5,6 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Fonts
-brew install --cask homebrew/cask-fonts/font-open-sans
-brew install --cask homebrew/cask-fonts/font-roboto
 brew install --cask homebrew/cask-fonts/font-hack
 brew install --cask homebrew/cask-fonts/font-impact
 brew install --cask homebrew/cask-fonts/font-public-sans
