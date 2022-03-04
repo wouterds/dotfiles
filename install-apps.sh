134626eb8c70a7d8e1afbaeb801b047fbc30bfc8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-03-04 20:34:12 +0100

    refactor(apps): pass --cask explicitly again
    Restores the `--cask` flag on all 35 app installs. Relying on Homebrew inferring the type from the
    name breaks as soon as a formula appears with the same name as a cask, and Homebrew warns about the
    ambiguity.

--- install-apps.sh
+++ install-apps.sh
@@ -5,41 +5,41 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Apps
-brew install android-studio
-brew install dropbox
-brew install google-chrome
-brew install keepingyouawake
-brew install docker
-brew install spotify
-brew install transmission
-brew install spectacle
-brew install viscosity
-brew install sketch
-brew install alfred
-brew install vlc
-brew install bartender
-brew install appcleaner
-brew install zoomus
-brew install telegram
-brew install discord
-brew install visual-studio-code
-brew install ledger-live
-brew install handbrake
-brew install imageoptim
-brew install microsoft-teams
-brew install notion
-brew install postman
-brew install tableplus
-brew install maccy
-brew install monitorcontrol
-brew install signal
-brew install tradingview
-brew install figma
-brew install timemachineeditor
-brew install flipper
-brew install coconutbattery
-brew install displaperture
-brew install muzzle
+brew install --cask android-studio
+brew install --cask dropbox
+brew install --cask google-chrome
+brew install --cask keepingyouawake
+brew install --cask docker
+brew install --cask spotify
+brew install --cask transmission
+brew install --cask spectacle
+brew install --cask viscosity
+brew install --cask sketch
+brew install --cask alfred
+brew install --cask vlc
+brew install --cask bartender
+brew install --cask appcleaner
+brew install --cask zoomus
+brew install --cask telegram
+brew install --cask discord
+brew install --cask visual-studio-code
+brew install --cask ledger-live
+brew install --cask handbrake
+brew install --cask imageoptim
+brew install --cask microsoft-teams
+brew install --cask notion
+brew install --cask postman
+brew install --cask tableplus
+brew install --cask maccy
+brew install --cask monitorcontrol
+brew install --cask signal
+brew install --cask tradingview
+brew install --cask figma
+brew install --cask timemachineeditor
+brew install --cask flipper
+brew install --cask coconutbattery
+brew install --cask displaperture
+brew install --cask muzzle
 brew install homebrew/cask-drivers/sonos
 brew install homebrew/cask-drivers/jabra-direct
 
