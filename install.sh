46ef365e29fbe89cb4c53d6afc57b97bb61dc5ba
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-03-04 14:20:23 +0100

    fix(install): use the current Homebrew installer command
    The Ruby-based installer URL was retired: Homebrew's install script is now bash and lives at
    install/HEAD/install.sh. The old command failed outright, and newer macOS no longer ships a system
    Ruby to run it with.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ./macos-prefs.sh
 
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install packages, drivers, apps & fonts
 ./install-packages.sh
