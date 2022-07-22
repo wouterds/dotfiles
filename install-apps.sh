75f62de1def326352656f16b7580c176c0b60e89
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-07-22 18:48:38 +0200

    :sparkles: Install Helium

--- install-apps.sh
+++ install-apps.sh
@@ -41,6 +41,7 @@ brew install --cask coconutbattery
 brew install --cask displaperture
 brew install --cask muzzle
 brew install --cask hot
+brew install --cask helium
 brew install homebrew/cask-drivers/sonos
 brew install homebrew/cask-drivers/jabra-direct
 
