7550d05057893e1246cda15b0875ebe8805058c4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-07-25 15:12:36 +0200

    refactor(apps): prune the app list and group it by purpose
    Removes sketch and Proxie, and adds proxyman in Proxie's place as the HTTP debugging proxy.
    
    Reorders the remaining casks so they read by purpose: the everyday tools first, then communication,
    media, utilities and monitoring.
    
    Also drops the two homebrew/cask-drivers entries: sonos becomes a plain cask, which it is now
    available as, and jabra-direct is removed. alfred goes back to the versioned alfred4 name.

--- install-apps.sh
+++ install-apps.sh
@@ -5,44 +5,41 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Apps
-brew install --cask android-studio
 brew install --cask dropbox
 brew install --cask google-chrome
-brew install --cask keepingyouawake
+brew install --cask visual-studio-code
+brew install --cask postman
 brew install --cask docker
-brew install --cask spotify
-brew install --cask transmission
+brew install --cask proxyman
+brew install --cask android-studio
+brew install --cask keepingyouawake
 brew install --cask spectacle
 brew install --cask viscosity
-brew install --cask sketch
-brew install --cask alfred
-brew install --cask vlc
+brew install --cask alfred4
 brew install --cask bartender
 brew install --cask appcleaner
-brew install --cask zoomus
 brew install --cask telegram
+brew install --cask signal
 brew install --cask discord
-brew install --cask visual-studio-code
+brew install --cask microsoft-teams
+brew install --cask zoomus
 brew install --cask ledger-live
+brew install --cask tradingview
+brew install --cask spotify
+brew install --cask sonos
+brew install --cask vlc
 brew install --cask handbrake
 brew install --cask imageoptim
-brew install --cask microsoft-teams
 brew install --cask notion
-brew install --cask postman
 brew install --cask tableplus
 brew install --cask maccy
 brew install --cask monitorcontrol
-brew install --cask signal
-brew install --cask tradingview
 brew install --cask figma
 brew install --cask timemachineeditor
 brew install --cask flipper
 brew install --cask coconutbattery
 brew install --cask displaperture
 brew install --cask muzzle
-brew install --cask hot
-brew install homebrew/cask-drivers/sonos
-brew install homebrew/cask-drivers/jabra-direct
 
 # Mac App Store apps
 mas install 443987910 # 1Password
@@ -58,7 +55,6 @@ mas install 409201541 # Pages
 mas install 409203825 # Numbers
 mas install 497799835 # Xcode
 mas install 640199958 # Developer
-mas install 897814548 # Proxie
 mas install 462054704 # Microsoft Word
 mas install 462058435 # Microsoft Excel
 mas install 462062816 # Microsoft PowerPoint
