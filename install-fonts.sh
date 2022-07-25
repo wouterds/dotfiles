e845b8ae4e3a6a121e4423a2bd0de94a56cf5474
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-07-25 15:22:33 +0200

    :fire: install less fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -6,8 +6,4 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Fonts
 brew install --cask homebrew/cask-fonts/font-hack
-brew install --cask homebrew/cask-fonts/font-impact
 brew install --cask homebrew/cask-fonts/font-public-sans
-brew install --cask homebrew/cask-fonts/font-raleway
-brew install --cask homebrew/cask-fonts/font-oswald
-brew install --cask homebrew/cask-fonts/font-inter
